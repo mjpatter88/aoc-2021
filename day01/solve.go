0eb4ed74c7459b88924f0a603c435104984353b3
Michael Patterson <mjpatter88@gmail.com>
2021-12-01 10:31:13 -0600

    Add solution to the second part of day 1.

--- day01/solve.go
+++ day01/solve.go
@@ -2,29 +2,61 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
-  "strconv"
 	"os"
+	"strconv"
 )
 
 func Solve(input []int) int {
-  // The only trick is that the first item should never count as increasing.
-  // To accomplish this, we just set the initial "lastMeasurement" value to
-  // be the first input value.
-  if len(input) == 0 {
-    return 0
-  }
-  numIncreasing := 0
-  lastMeasurement := input[0]
+	// The only trick is that the first item should never count as increasing.
+	// To accomplish this, we just set the initial "lastMeasurement" value to
+	// be the first input value.
+	if len(input) == 0 {
+		return 0
+	}
+	numIncreasing := 0
+	lastMeasurement := input[0]
 
-  for _, value := range input {
-    if value > lastMeasurement {
-      numIncreasing += 1
-    }
-    lastMeasurement = value
-  }
+	for _, value := range input {
+		if value > lastMeasurement {
+			numIncreasing += 1
+		}
+		lastMeasurement = value
+	}
 	return numIncreasing
+}
+
+func Solve2(input []int) int {
+	// In this case we need > 3 elements to have at least two windows to compare
+	if len(input) <= 3 {
+		return 0
+	}
+	numIncreasing := 0
+	newSum := 0
+	lastSum, err := CalcWindowSum(input, 0)
+	index := 1
+
+	for err == nil {
+		newSum, err = CalcWindowSum(input, index)
+		if err == nil && newSum > lastSum {
+			numIncreasing += 1
+		}
+		lastSum = newSum
+		index += 1
+	}
+	return numIncreasing
+}
+
+// Returns the sum of a three-measurement window starting at the given index.
+// Returns an error if there are not enough input elements.
+func CalcWindowSum(input []int, startingIndex int) (int, error) {
+	maxStartingIndex := len(input) - 3
+	if startingIndex > maxStartingIndex {
+		return 0, errors.New("Not enough elements to calculate three-measurement window.")
+	}
+	return input[startingIndex] + input[startingIndex+1] + input[startingIndex+2], nil
 }
 
 func main() {
@@ -34,22 +66,25 @@ func main() {
 	}
 	defer file.Close()
 
-  var input []int
+	var input []int
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-    numString := scanner.Text()
-    num, err := strconv.Atoi(numString)
-    if err != nil {
-      log.Fatal(err)
-    }
-    input = append(input, num)
+		numString := scanner.Text()
+		num, err := strconv.Atoi(numString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = append(input, num)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-  answer := Solve(input)
-  fmt.Println(answer)
+	answer := Solve(input)
+	fmt.Println(answer)
+
+	answer2 := Solve2(input)
+	fmt.Println(answer2)
 }
--- day01/solve_test.go
+++ day01/solve_test.go
@@ -4,23 +4,63 @@ import (
 	"testing"
 )
 
+var input = []int{
+	199,
+	200,
+	208,
+	210,
+	200,
+	207,
+	240,
+	269,
+	260,
+	263,
+}
+
 func TestSolve(t *testing.T) {
-	input := []int{
-		199,
-		200,
-		208,
-		210,
-		200,
-		207,
-		240,
-		269,
-		260,
-		263,
-	}
 	got := Solve(input)
 	want := 7
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func TestSolve2(t *testing.T) {
+	got := Solve2(input)
+	want := 5
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestCalcWindowSum(t *testing.T) {
+	t.Run("Calc window at 0", func(t *testing.T) {
+		got, err := CalcWindowSum(input, 0)
+		want := input[0] + input[1] + input[2]
+		if err != nil {
+			t.Errorf("got an unexpected error")
+		}
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Calc window in middle", func(t *testing.T) {
+		got, err := CalcWindowSum(input, 3)
+		want := input[3] + input[4] + input[5]
+		if err != nil {
+			t.Errorf("got an unexpected error")
+		}
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("Calc window not enough elements", func(t *testing.T) {
+		_, err := CalcWindowSum(input, 8)
+		if err == nil {
+			t.Errorf("expected an error but did not get one")
+		}
+	})
 }
